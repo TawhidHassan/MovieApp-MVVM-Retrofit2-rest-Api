4dfc993deaa1a72397ed54e44ae8faf878a85120
sifat hassan <sifat007.sh7@gmail.com>
2021-06-09 18:53:11 +0600

    add dependence retrofit and make pacakge for utils

---
+++ app/src/main/java/com/example/movieappmvvmretrofit/Utils/Credentials.java
@@ -0,0 +1,9 @@
+package com.example.movieappmvvmretrofit.Utils;
+
+public class Credentials {
+
+    public static final  String BASE_URL="https://api.themoviedb.org";
+
+    public static final  String API_KEY="dd1c3c69346180699bdb9d6aa48ff8a7";
+
+}
